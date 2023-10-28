834dafa499e46afe0c7c4ed786e2c7d4f21f745c
ali elsadany <alieelsadany@gmail.com>
2023-10-28 15:42:42 +0200

    check logo

--- lib/core/utils/app_assets.dart
+++ lib/core/utils/app_assets.dart
@@ -7,5 +7,5 @@ class AppAssets{
   static const String onBoard1 = '${basePath}onboard1.png';
   static const String onBoard2 = '${basePath}onboard2.png';
   static const String onBoard3 = '${basePath}onboard3.png';
-  static const String check = '${basePath}rightcheck.png';
+  static const String check = '${basePath}check.png';
 }
