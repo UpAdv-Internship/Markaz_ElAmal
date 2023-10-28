0a2b533af27b506d0cb8aaae269c2b9984878884
ali elsadany <alieelsadany@gmail.com>
2023-10-28 15:35:53 +0200

    App Assetes

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,11 @@
+class AppAssets{
+  static const String basePath = 'assets/images/';
+  static const String appLogo = '${basePath}Logo.png';
+  static const String whiteBackground = '${basePath}backgroundone.png';
+  static const String whiteFrameBackground = '${basePath}backgroundtwo.png';
+  static const String coloredBackground = '${basePath}backgroundthree.png';
+  static const String onBoard1 = '${basePath}onboard1.png';
+  static const String onBoard2 = '${basePath}onboard2.png';
+  static const String onBoard3 = '${basePath}onboard3.png';
+  static const String check = '${basePath}rightcheck.png';
+}
