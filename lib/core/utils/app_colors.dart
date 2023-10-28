1ef334d4b1e7a74194868d0797a82c0d1685550e
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-10-28 16:30:18 +0200

    Text Style Part Two

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
 
-class AppColors{
-
+class AppColors {
   static const Color primary = Color(0xff336EA6);
   static const Color secondary = Color(0xff233B55);
   static const Color white = Color(0xffFFFFFF);
   static const Color markedStars = Color(0xffFFE665);
   static const Color unMarkedStars = Color(0xffD9D9D9);
-
-
-
+  static const Color lightGrey = Color(0xffBDCAD6);
 }
--- lib/core/utils/app_text_styles.dart
+++ lib/core/utils/app_text_styles.dart
@@ -8,6 +8,12 @@ abstract class CustomTextStyle {
     fontWeight: FontWeight.w400,
     fontSize: 24,
   );
+  static const peralta400White16 = TextStyle(
+    fontFamily: "Peralta",
+    color: AppColors.white,
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+  );
   static const poppins700White24 = TextStyle(
     fontFamily: "Poppins",
     color: AppColors.white,
@@ -39,5 +45,74 @@ abstract class CustomTextStyle {
     color: AppColors.secondary,
     fontWeight: FontWeight.w500,
     fontSize: 24,
+  );
+  static const poppins600White24 = TextStyle(
+    fontFamily: "Poppins",
+    color: AppColors.white,
+    fontWeight: FontWeight.w600,
+    fontSize: 24,
+  );
+  static const poppins400White16 = TextStyle(
+    fontFamily: "Poppins",
+    color: AppColors.white,
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+  );
+
+  static const poppins600primary24 = TextStyle(
+    fontFamily: "Poppins",
+    color: AppColors.primary,
+    fontWeight: FontWeight.w600,
+    fontSize: 24,
+  );
+
+  static const poppins600White16 = TextStyle(
+    fontFamily: "Poppins",
+    color: AppColors.white,
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+  );
+  static const poppins600primary16 = TextStyle(
+    fontFamily: "Poppins",
+    color: AppColors.primary,
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+  );
+  static const poppins600secondary16 = TextStyle(
+    fontFamily: "Poppins",
+    color: AppColors.secondary,
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+  );
+  static const poppins400secondary16 = TextStyle(
+    fontFamily: "Poppins",
+    color: AppColors.secondary,
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+  );
+  static const poppins400secondary20 = TextStyle(
+    fontFamily: "Poppins",
+    color: AppColors.secondary,
+    fontWeight: FontWeight.w400,
+    fontSize: 20,
+  );
+  static const poppins500primary16 = TextStyle(
+    fontFamily: "Poppins",
+    color: AppColors.primary,
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+  );
+  static const poppins800secondary40 = TextStyle(
+    fontFamily: "Poppins",
+    color: AppColors.secondary,
+    fontWeight: FontWeight.w800,
+    fontSize: 40,
+  );
+
+  static const poppins500lightGrey14 = TextStyle(
+    fontFamily: "Poppins",
+    color: AppColors.lightGrey,
+    fontWeight: FontWeight.w500,
+    fontSize: 14,
   );
 }
