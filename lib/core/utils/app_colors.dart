d077c94d08f7407ee3f9b0a98d2d77b389034bc1
ali elsadany <alieelsadany@gmail.com>
2023-10-28 15:55:28 +0200

    More Colors added

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -7,6 +7,12 @@ class AppColors{
   static const Color white = Color(0xffFFFFFF);
   static const Color markedStars = Color(0xffFFE665);
   static const Color unMarkedStars = Color(0xffD9D9D9);
+  // Setting
+  static const Color profileInfo = Color(0xffA6D6FF);
+  static const Color privacy = Color(0xff9BA6FA);
+  static const Color changePass = Color(0xff00C48C);
+  static const Color rateApp = Color(0xffF6BB86);
+  static const Color feedback = Color(0xffFFDF92);
 
 
 
