fe0e8a70f51c6d4cb8dc3d824e78d8c4a583195a
Mohammed Saeed <105430093+SA33ED@users.noreply.github.com>
2023-11-02 15:44:47 +0200

    const

--- lib/features/home/cubit/home_cubit.dart
+++ lib/features/home/cubit/home_cubit.dart
@@ -1,6 +1,3 @@
-
-
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:markaz_elamal/features/booking/presentation/screens/doctor_profile.dart';
@@ -10,11 +7,16 @@ import 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
   HomeCubit() : super(HomeInitial());
-  
-  List<Widget> screens=[const HomeScreen(),const DoctorProfile(),HomeScreen(),HomeScreen()];
+
+  List<Widget> screens = [
+    const HomeScreen(),
+    const DoctorProfile(),
+    const HomeScreen(),
+    const HomeScreen()
+  ];
   int currenIndex = 0;
-  void changeIndex(index){
-    currenIndex=index;
+  void changeIndex(index) {
+    currenIndex = index;
     emit(CurrenIndexState());
   }
 }
